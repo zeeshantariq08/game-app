f877bbcf4874ffe4c3016758a81d10fae1f1bf9f
zeeshan <zeeshan.tariq@quantum-h.com>
2024-07-17 00:07:31 +0500

    show message if no items found

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,6 +1,5 @@
 function ListGroup() {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-    items = [];
 
     return (
         <>
@@ -9,8 +8,8 @@ function ListGroup() {
                 items.length == 0 && <p>No items found</p>
             }
             <ul className="list-group">
-                {items.map((item) => (
-                    <li key={item}>{item}</li>
+                {items.map((item, index) => (
+                    <li onClick={(event) => console.log(event)} className="list-group-item" key={item}>{item}</li>
                 ))}
             </ul>
         </>
