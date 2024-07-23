73c6be170bf6ef69ca58b6d649b2cd8033a37d0a
zeeshan <zeeshan.tariq@quantum-h.com>
2024-07-23 23:30:34 +0500

    using children props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,17 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
     const handleSelectItem = (item: string) => {
         console.log(item);
     }
-    return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+    return <div>
+        <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+        <Alert>
+            <h1>Hello</h1>
+        </Alert>
+    </div>
 }
 
 export default App;
